953f0d7f05c667bb26372ef6a1c9f0b5799f9e03
Lwandile <100771485+lwandile9@users.noreply.github.com>
2024-11-20 10:09:22 +0200

    Update BlogPost.jsx

--- client/src/components/BlogPost.jsx
+++ client/src/components/BlogPost.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import '../css/fetchBlogInit.css'; // relative path from components to css
+import './css/fetchBlogInit.css';
+
 
 
 const BlogPost = () => {
